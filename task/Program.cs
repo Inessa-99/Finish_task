6e672ca3b97b2b8ffd555c097b79066a9f83e3da
Inessa <inesa-99@mail.ru>
2022-09-09 09:33:06 +0300

    task ready

---
+++ task/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] array = new string[4] { "1234", "1567", "-2", "computer science"};
+string[] arrayLess3 = new string[array.Length]; 
+
+CreateNewArrayLess3(array, arrayLess3);
+PrintArray(arrayLess3);
+
+void CreateNewArrayLess3(string[] array1, string[] array2)
+{
+    int n = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[n] = array1[i];
+            n++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
